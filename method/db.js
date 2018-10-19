ac212a6a46d3f534a85f40754cb6cc3de8237368
Bukovski <kmanankov@bk.ru>
2018-10-19 17:37:45 +0300

    promise request

--- method/db.js
+++ method/db.js
@@ -7,7 +7,7 @@ const db = {};
 const dataDir = path.join(__dirname, '../.data/users.json');
 
 function generateData(id) {
-  const fakeData = {
+  return {
     "id": id,
     "name": faker.name.firstName(),
     "username": faker.internet.userName(),
@@ -15,71 +15,77 @@ function generateData(id) {
     "email": faker.internet.email(),
     "phone": faker.phone.phoneNumberFormat(1)
   };
-  
-  return fakeData;
 }
 
 db.parseJsonToObject = (str) => {
   try {
-    const obj = JSON.parse(str);
-    
-    return obj;
+    return JSON.parse(str);
   } catch(e) {
     return {};
   }
 };
 
-db.createJson = (data, callback) => {
-  fs.open(dataDir, 'wx', (err, fileDescriptor) => {
-    if (err && !fileDescriptor) return callback('Could not create new file, it may already exist');
-    const stringData = JSON.stringify(data, undefined, 2);
+db.createJson = (data) => {
+  return new Promise((resolve, reject) => {
+    fs.open(dataDir, 'wx', (err, fileDescriptor) => {
+      if (err && !fileDescriptor) return reject('Could not create new file, it may already exist');
+      const stringData = JSON.stringify(data, undefined, 2);
     
-    fs.writeFile(fileDescriptor, stringData, (err) => {
-      if (err) return callback('Error writing to new file');
-      
-      fs.close(fileDescriptor, (err) => {
-        if (err) return callback('Error closing new file');
-        
-        callback(false);
-      });
-    });
-  });
-};
-
-db.updateJson = (data, callback) => {
-  fs.open(dataDir, 'r+', (err, fileDescriptor) => {
-    if (err && !fileDescriptor) return callback('Could not open file for updating, it may not exist yet');
-    const stringData = JSON.stringify(data, undefined, 2);
-    
-    fs.ftruncate(fileDescriptor, (err) => {
-      if (err) return callback('Error truncating file');
-      
       fs.writeFile(fileDescriptor, stringData, (err) => {
-        if (err) return callback('Error writing to existing file');
-        
+        if (err) return reject('Error writing to new file');
+      
         fs.close(fileDescriptor, (err) => {
-          if (err) return callback('Error closing existing file');
-          
-          callback(false);
+          if (err) return reject('Error closing new file');
+  
+          resolve(true)
         });
       });
     });
   });
 };
 
-db.readJson = (callback) => {
-  fs.readFile(dataDir, 'utf8', (err, data) => {
-    if(err && !data) return callback(err, data);
-    
-    const parsedData = db.parseJsonToObject(data);
-    callback(false, parsedData);
+db.updateJson = (data) => {
+  return new Promise((resolve, reject) => {
+    fs.open(dataDir, 'r+', (err, fileDescriptor) => {
+      if (err && !fileDescriptor) return reject('Could not open file for updating, it may not exist yet');
+      const stringData = JSON.stringify(data, undefined, 2);
+      
+      fs.ftruncate(fileDescriptor, (err) => {
+        if (err) return reject('Error truncating file');
+        
+        fs.writeFile(fileDescriptor, stringData, (err) => {
+          if (err) return reject('Error writing to existing file');
+          
+          fs.close(fileDescriptor, (err) => {
+            if (err) return reject('Error closing existing file');
+  
+            resolve(true)
+          });
+        });
+      });
+    });
   });
 };
 
-db.deleteJson = (callback) => {
-  fs.unlink(dataDir, (err) => {
-    callback(err);
+db.readJson = () => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(dataDir, 'utf8', (err, data) => {
+      if(err || !data) return reject(err, data);
+    
+      const parsedData = db.parseJsonToObject(data);
+      resolve(parsedData);
+    });
   });
+};
+
+db.deleteJson = () => {
+  return new Promise((resolve, reject) => {
+    fs.unlink(dataDir, (err) => {
+      if (err) return reject(err);
+  
+      resolve(true)
+    });
+  })
 };
 
 db.generateUserJson = (count) => {
@@ -89,13 +95,14 @@ db.generateUserJson = (count) => {
     users = users.concat(generateData(i));
   }
   
-  db.createJson(users, (err) => {
-    if (err) db.updateJson(users, (error) => {
-      if (error) console.log(error);
-    });
-  });
-  
-  return null;
+  return new Promise((resolve, reject) => {
+    db.createJson(users)
+      .then(() => resolve(true))
+      .catch(() => db.updateJson(users)
+        .then(() => resolve(true))
+        .catch((error) => reject(error))
+      )
+  })
 };
 
 
--- method/requset.js
+++ method/requset.js
@@ -4,98 +4,91 @@ const { notFound, success } = require('./response');
 
 const request = {};
 
-request.recordCreateList = function(res, count) {
-  const createRecords = db.generateUserJson(+count);
-  
-  if (createRecords) return notFound(res, `Records not create`);
-  
-  success(res, `${ count } records was created`);
+request.recordCreateList = (res, count) => {
+  db.generateUserJson(+count)
+    .then(() => success(res, `${ count } records was created`))
+    .catch(() => notFound(res, `Records not create`))
 };
 
-request.getUsersId = function(res, id) {
-  db.readJson((err, arr) => {
-    if (err) return notFound(res, err);
-    
-    const onlyOne = arr.filter(item => item.id === +id);
-    
-    success(res, onlyOne, true);
-  });
-};
-
-request.postUsers = function(res) {
-  db.readJson((err, arr) => {
-    if (err) return notFound(res, err);
-    
-    success(res, arr, true);
-  });
-};
-
-request.deleteUsers = function(res, id) {
-  db.readJson((err, arr) => {
-    if (err) return notFound(res, err);
-    
-    let isExists = false;
-    
-    const withoutOne = arr.filter(item => {
-      if (item.id === +id) {
-        isExists = true;
-      }
+request.getUsersId = (res, id) => {
+  db.readJson()
+    .then((arr) => {
+      const onlyOne = arr.filter(item => item.id === +id);
       
-      return item.id !== +id
-    });
-    
-    db.updateJson(withoutOne, (err) => {
-      if (err) return notFound(res, err);
+      success(res, onlyOne, true);
+    })
+    .catch((err) => notFound(res, err))
+};
+
+request.postUsers = (res) => {
+  db.readJson()
+    .then((arr) => success(res, arr, true))
+    .catch((err) => notFound(res, err))
+};
+
+request.deleteUsers = (res, id) => {
+  db.readJson()
+    .then((arr) => {
+      let isExists = false;
+      
+      const withoutOne = arr.filter(item => {
+        if (item.id === +id) {
+          isExists = true;
+        }
+        
+        return item.id !== +id
+      });
+      
       if (!isExists) return notFound(res, `Record #${ id } not exists`);
       
-      success(res, `Record #${ id } was deleted`);
+      db.updateJson(withoutOne)
+        .then(() => {
+          return success(res, `Record #${ id } was deleted`)
+        })
+        .catch((err) => notFound(res, err))
     })
-  });
+    .catch((err) => notFound(res, err));
 };
 
-request.putUsersBody = function(res, body) {
-  db.readJson((err, arr) => {
-    if (err) return notFound(res, err);
-    
-    let lastId = arr[ arr.length - 1 ].id;
-    body.id = lastId + 1;
-    
-    const newData = arr.concat(body);
-    
-    db.updateJson(newData, (err) => {
-      if (err) return notFound(res, err);
+request.putUsersBody = (res, body) => {
+  db.readJson((arr) => {
+      let lastId = arr[ arr.length - 1 ].id;
       
-      success(res, 'Record was added');
+      body.id = lastId + 1;
+      
+      const newData = arr.concat(body);
+      
+      db.updateJson(newData)
+        .then(() => success(res, 'Record was added'))
+        .catch((err) => notFound(res, err));
     })
-  });
+    .catch((err) => notFound(res, err))
 };
 
-request.pathUsersIdBody = function(res, id, body) {
-  db.readJson((err, arr) => {
-    if (err) return notFound(res, err);
-    
-    let indexRecord = 0;
-    
-    arr.every((value, index) => {
-      if (value.id === +id) {
-        indexRecord = index;
+request.pathUsersIdBody = (res, id, body) => {
+  db.readJson()
+    .then((arr) => {
+      let indexRecord = 0;
+      
+      arr.every((value, index) => {
+        if (value.id === +id) {
+          indexRecord = index;
+          
+          return false
+        }
         
-        return false
-      }
+        return true;
+      });
       
-      return true;
-    });
-    
-    const dataRecord = arr[ indexRecord ];
-    
-    arr[ indexRecord ] = Object.assign({}, dataRecord, body);
-    
-    db.updateJson(arr, (err) => {
-      if (err) return notFound(res, err);
+      const dataRecord = arr[ indexRecord ];
       
-      success(res, 'Record was changed');
-    });
-  });
+      arr[ indexRecord ] = Object.assign({}, dataRecord, body);
+      
+      db.updateJson(arr)
+        .then(() => success(res, 'Record was changed'))
+        .catch((err) => notFound(res, err))
+    })
+    .catch((err) => notFound(res, err))
 };
 
 
